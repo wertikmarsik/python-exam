23ada6a760e96097dd25232c6d9967e259b29ead
Svyatoslal <wertik.marsik@gmail.com>
2023-12-14 16:11:05 +0200

    finish

---
+++ exam.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+def convert_date(input_date, input_format, output_format):
+    try:
+        # Парсимо вхідну дату за вказаним форматом
+        parsed_date = datetime.strptime(input_date, input_format)
+        
+        # Форматуємо дату за вказаним вихідним форматом
+        output_date = parsed_date.strftime(output_format)
+        
+        return output_date
+    
+    except ValueError as e:
+        return f"Помилка: {e}"
+
+# Приклад використання
+input_date = input("Введіть дату: ")
+input_format = '%d/%m/%Y'
+output_format_1 = '%Y-%m-%d'
+output_format_2 = '%m-%d-%Y'
+output_format_3 = '%d-%m-%Y'
+
+result_1 = convert_date(input_date, input_format, output_format_1)
+result_2 = convert_date(input_date, input_format, output_format_2)
+result_3 = convert_date(input_date, input_format, output_format_3)
+
+print(result_1)
+print(result_2)
+print(result_3)
